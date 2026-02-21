996e785a144ec5fe31458b1427c28c32026f888d
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-21 18:13:40 +0530

    implement logout in the route, update the controller part

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -144,3 +144,24 @@ export const login = async (req, res) => {
     }
 }
 
+
+// Logout Controller
+export const logout = async (req, res) => {
+
+  try {
+    res.clearCookie("accessToken")
+    res.clearCookie("refreshToken")
+
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
+
+
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express"
 import {
   register,
-  login
+  login,
+  logout
 } from "../controllers/auth.controller.js"
+import authenticateRoute from "../middleware/auth.middleware.js"
 import { upload } from "../middleware/upload.middleware.js"
 
 // Initialize the router
@@ -11,7 +13,7 @@ const router = express.Router()
 // Use routers
 router.post("/register", upload.single("profilePic"), register)
 router.post("/login", login)
-
+router.post("/logout", authenticateRoute, logout)
 
 
 export default router
