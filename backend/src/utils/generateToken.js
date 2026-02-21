da235b639ceb6edf93c013860bca3c966cf77ab5
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-21 17:57:44 +0530

    generate access and refersh token

---
+++ backend/src/utils/generateToken.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken"
+
+export const generateAccessToken = (userId) => {
+    return jwt.sign(
+        { userId },
+        process.env.ACCESS_TOKEN,
+        { expiresIn: "15m" }
+    )
+}
+
+export const generateRefreshToken = (userId) => {
+    return jwt.sign(
+        { userId },
+        process.env.REFRESH_TOKEN,
+        { expiresIn: "7d" }
+    )
+}
+
+export const generateTokens = (userId) => {
+    const accessToken = generateAccessToken(userId)
+    const refreshToken = generateRefreshToken(userId)
+    return { accessToken, refreshToken }
+}
