cfcba38ba164847ac1e7b509b2df2648ec0e6caa
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-21 12:38:42 +0530

    creating user model

---
+++ backend/src/modules/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    otp: {
+      type: String,
+    },
+    otpExpire: {
+      type: Date,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema)
