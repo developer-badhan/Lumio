2d710cce11f46182f3ade44feb53b8cde6cc89b6
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-21 11:49:11 +0530

    start to build routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-import dotenv from "dotenv";
-import app from "./src/app.js";
-import connectDB from "./src/config/db.js";
+import dotenv from "dotenv"
+import app from "./src/app.js"
+import connectDB from "./src/config/db.js"
+
 
 // Load environment variables
 dotenv.config();
