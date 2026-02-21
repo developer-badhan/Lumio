7f10a23e3ed7b217a119c7ae5822b88c0693dbb2
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-21 12:40:26 +0530

    generating all routes and their methods

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,16 @@
+import express from "express"
+import {
+  register,
+  verifyOtp,
+  login,
+  logout,
+} from "../controllers/auth.controller.js"
+
+const router = express.Router()
+
+router.post("/register", register)
+router.post("/verify-otp", verifyOtp)
+router.post("/login", login)
+router.post("/logout", logout)
+
+export default router
