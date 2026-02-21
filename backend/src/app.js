8c82fb781c83c6c9b7534b3df991afa77ee4bc12
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-21 11:48:43 +0530

    implement cors for sharing resource to the frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import cors from "cors"
+import errorHandler from "./src/middleware/errorMiddleware.js"
 
 // Import the  Routes
-
+// import authRouter from "./routes/auth.route.js";
 
 // Initialize Express App
 const app = express()
@@ -11,8 +13,20 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-// Use the  Routes
 
+// Error Handling Middleware
+app.use(errorHandler)
+
+
+// Resource Sharing Middleware
+app.use(cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true,
+  })
+)
+
+// Use the  Routes
+// app.use("/api/auth", authRouter);
 
 
 
