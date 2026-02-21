03a0be7ae8175dc7dc6476b2c791e3f892a68110
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-21 12:49:20 +0530

    add authorized checking via auth middleware and extend it in routes for protection

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,12 +5,14 @@ import {
   login,
   logout,
 } from "../controllers/auth.controller.js"
+import authorized from "../middleware/auth.middleware.js"
+
 
 const router = express.Router()
 
-router.post("/register", register)
-router.post("/verify-otp", verifyOtp)
-router.post("/login", login)
+router.post("/register", authorized, register)
+router.post("/verify-otp", authorized, verifyOtp)
+router.post("/login", authorized, login)
 router.post("/logout", logout)
 
 export default router
