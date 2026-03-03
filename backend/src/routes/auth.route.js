077321544eb565243e01d37b5c131582db64cf8e
Badhan Sarkar <badhansarkar164@gmail.com>
2026-03-03 21:00:42 +0530

    fully updated the auth controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -11,14 +11,14 @@ import {
   changeProfilePic,
   changePassword,
 } from "../controllers/auth.controller.js"
-import { upload } from "../middleware/upload.middleware.js"
+import { profileMediaUpload } from "../middleware/upload.middleware.js"
 import authenticateRoute from "../middleware/auth.middleware.js"
 
 // Initialize the router
 const router = express.Router()
 
 // Public Routes
-router.post("/register", upload.single("profilePic"), register)
+router.post("/register", profileMediaUpload, register)
 router.post("/verify-otp", verifyOtp)
 router.post("/otp-resend", otpResend)
 router.post("/login", login)
@@ -28,7 +28,7 @@ router.post("/refresh-token", refreshToken)
 // Protected Routes
 router.get("/me", authenticateRoute, getMe)
 router.get("/users", authenticateRoute, getAllUsers)
-router.patch("/change-profile-pic", authenticateRoute, upload.single("profilePic"), changeProfilePic)
+router.patch("/change-profile-pic", authenticateRoute, profileMediaUpload, changeProfilePic)
 router.patch("/change-password", authenticateRoute, changePassword)
 
 export default router
