d31c37981f5e4d4d223520e62694b666fc0c553c
Badhan Sarkar <badhansarkar164@gmail.com>
2026-03-03 13:04:29 +0530

    Create Notification Schema

---
+++ backend/src/models/notification.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose"
+
+const notificationSchema = new mongoose.Schema({
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  type: {
+    type: String,
+    enum: ["message", "group-add", "mention"],
+    required: true
+  },
+  conversation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Conversation"
+  },
+  message: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Message"
+  },
+  isRead: {
+    type: Boolean,
+    default: false
+  }
+
+}, { timestamps: true })
+
+// Improve Schema for Performance
+notificationSchema.index({ recipient: 1, isRead: 1, createdAt: -1 })
+
+export default mongoose.model("Notification", notificationSchema)
