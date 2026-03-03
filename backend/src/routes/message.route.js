b50f902413bcf03e0a1d8f29661530006ec2cbb3
Badhan Sarkar <badhansarkar164@gmail.com>
2026-03-03 20:06:36 +0530

    add media upload on message post

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,12 +6,13 @@ import {
   deleteMessage
 } from "../controllers/message.controller.js"
 import authMiddleware from "../middleware/auth.middleware.js"
+import { mediaUpload } from "../middleware/upload.middleware.js"
 
 // Initialize the router
 const router = express.Router()
 
 // Protected Routes
-router.post("/", authMiddleware, sendMessage)
+router.post("/", authMiddleware, mediaUpload, sendMessage)
 router.get("/:conversationId", authMiddleware, getMessages)
 router.patch("/edit/:messageId", authMiddleware, editMessage)
 router.delete("/delete/:messageId", authMiddleware, deleteMessage)
